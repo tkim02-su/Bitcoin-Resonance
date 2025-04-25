b47278348dc5025a54d6a28935baa280c6ac2cf5
Taeuk Kim <taeuk.kim@encora.com>
2025-04-25 17:14:08 -0400

    Websocket

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,17 @@
 // app/page.tsx
 'use client';
 
-import { useEffect, useState } from 'react';
-import { fetchBitcoinData } from '../lib/fetchBitcoin';
 import BreathingPanel from './components/BreathingPanel';
 import MarketStats from './components/MarketStats';
 import dynamic from 'next/dynamic';
 import AboutSection from './components/AboutSection';
 import TransactionFeed from './components/TransactionFeed';
+import useBitcoinWebSocket from './hooks/useBitcoinWebSocket';
 
 const BitcoinUniverse3D = dynamic(() => import('./components/BitcoinUniverse3D'), { ssr: false });
 
 export default function Home() {
-  const [btcData, setBtcData] = useState<{
-    price: number;
-    volume: number;
-    change: number;
-  } | null>(null);
-
-  useEffect(() => {
-    const load = async () => {
-      try {
-        const data = await fetchBitcoinData();
-        setBtcData(data);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    load();
-
-    const interval = setInterval(load, 15000); // every 15 seconds
-    return () => clearInterval(interval);
-  }, []);
+  const btcData = useBitcoinWebSocket();
 
   return (
     <main className="relative flex flex-col items-center justify-center min-h-screen bg-black text-white overflow-x-hidden">
@@ -53,20 +32,22 @@ export default function Home() {
           <div className="mt-4 text-center text-lg space-y-1 z-10">
             <p>
               Price: $
-              {Number(btcData.price.toFixed(2)).toLocaleString(undefined, {
+              {btcData.price.toLocaleString(undefined, {
                 minimumFractionDigits: 2,
                 maximumFractionDigits: 2,
               })}
             </p>
             <p>
               Volume: $
-              {Number(btcData.volume.toFixed(2)).toLocaleString(undefined, {
+              {btcData.volume.toLocaleString(undefined, {
                 minimumFractionDigits: 2,
                 maximumFractionDigits: 2,
               })}
             </p>
             <p
-              className={btcData.change >= 0 ? 'text-green-400' : 'text-red-400'}
+              className={
+                btcData.change >= 0 ? 'text-green-400' : 'text-red-400'
+              }
             >
               Change: {btcData.change.toFixed(2)}%
             </p>
@@ -75,8 +56,9 @@ export default function Home() {
           <div className="mt-20">
             <AboutSection />
           </div>
+
           <div className="mt-20">
-            <TransactionFeed /> 
+            <TransactionFeed />
           </div>
         </>
       ) : (
